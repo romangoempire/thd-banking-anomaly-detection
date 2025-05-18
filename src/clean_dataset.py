cd366713ae173588b8a8b38e2d52ae9658d4c9cc
RomanGoEmpire <71276897+RomanGoEmpire@users.noreply.github.com>
2025-05-18 12:55:06 +0200

    migrate to polars

--- src/clean_dataset.py
+++ src/clean_dataset.py
@@ -1,29 +1,33 @@
-import pandas as pd
+import polars as pl
 
 ORIGNAL_PATH: str = "data/original.csv"
 
 
-def remove_white_spaces(df: pd.DataFrame, col: str) -> pd.DataFrame:
-    df[col] = df[col].apply(lambda d: " ".join(d.split()) if isinstance(d, str) else d)
+def remove_white_spaces(df: pl.DataFrame, col: str) -> pl.DataFrame:
+    df = df.with_columns((pl.col(col).str.replace_all(r"\s+", " ")))
     return df
 
 
-def split_date_into_seperate_columns(df: pd.DataFrame, col: str) -> pd.DataFrame:
-    df.insert(2, f"{col}_year", df[col].dt.year)
-    df.insert(3, f"{col}_month", df[col].dt.month)
-    df.insert(4, f"{col}_day", df[col].dt.day)
-    df.insert(5, f"{col}_hour", df[col].dt.hour)
-    df.insert(6, f"{col}_minute", df[col].dt.minute)
-    df.insert(7, f"{col}_second", df[col].dt.second)
-    df.insert(8, f"{col}_weekday", df[col].dt.weekday)
+def split_date_into_seperate_columns(df: pl.DataFrame, col: str) -> pl.DataFrame:
+    df = df.with_columns(
+        [
+            (pl.col(col).dt.year()).alias(f"{col[0]}_year"),
+            (pl.col(col).dt.month()).alias(f"{col[0]}_month"),
+            (pl.col(col).dt.day()).alias(f"{col[0]}_day"),
+            (pl.col(col).dt.hour()).alias(f"{col[0]}_hour"),
+            (pl.col(col).dt.minute()).alias(f"{col[0]}_minute"),
+            (pl.col(col).dt.second()).alias(f"{col[0]}_second"),
+            (pl.col(col).dt.weekday()).alias(f"{col[0]}_weekday"),
+        ]
+    )
 
     return df
 
 
 def main() -> None:
-    df = pd.read_csv(ORIGNAL_PATH)
+    df = pl.read_csv(ORIGNAL_PATH)
     df = df.rename(
-        columns={
+        {
             "AGENCY": "agency",
             "TRANSACTION_DATE": "date",
             "TRANSACTION_AMOUNT": "amount",
@@ -35,21 +39,51 @@ def main() -> None:
     )
 
     df = remove_white_spaces(df, "agency")
-    df["vendor_name"] = df["vendor_name"].str.replace("*", "")
     df = remove_white_spaces(df, "vendor_name")
     df = remove_white_spaces(df, "vendor_state_province")
     df = remove_white_spaces(df, "description")
+    df = df.with_columns((pl.col("vendor_name").str.replace_all(r"\*", "")))
 
-    df["date"] = pd.to_datetime(df["date"])
-    df["modify_date"] = pd.to_datetime(df["modify_date"])
-    df.insert(2, "seconds_between", (df["modify_date"] - df["date"]).dt.total_seconds())
+    df = df.with_columns(
+        [
+            pl.col("modify_date").str.strptime(pl.Datetime, "%Y/%m/%d %H:%M:%S+00"),
+            pl.col("date").str.strptime(pl.Datetime, "%Y/%m/%d %H:%M:%S+00"),
+        ]
+    )
+    df = df.with_columns(
+        (pl.col("modify_date") - pl.col("date")).dt.total_seconds().alias("seconds")
+    )
     df = split_date_into_seperate_columns(df, "modify_date")
     df = split_date_into_seperate_columns(df, "date")
 
-    df = df.sort_values(["agency", "date"])
+    df = df.sort(["agency", "date"])
 
-    df = df.drop(columns=["OBJECTID", "date", "modify_date"])
-    df.to_csv("data/clean.csv", index=False)
+    df = df.drop(["OBJECTID"])
+    df = df.select(
+        [
+            "agency",
+            "d_year",
+            "d_month",
+            "d_day",
+            "d_hour",
+            "d_minute",
+            "d_second",
+            "d_weekday",
+            "m_year",
+            "m_month",
+            "m_day",
+            "m_hour",
+            "m_minute",
+            "m_second",
+            "m_weekday",
+            "seconds",
+            "amount",
+            "vendor_name",
+            "vendor_state_province",
+            "description",
+        ]
+    )
+    df.write_csv("data/clean.csv")
 
 
 if __name__ == "__main__":
